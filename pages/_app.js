89592a652145e330d074f0c18f94c566012d074e
Sabastian Highton <shighton@Sabastians-MacBook-Air.local>
2023-01-03 16:20:51 -0500

    fist commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-import '../styles/globals.css'
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
+import {Layout} from '../components';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles/globals.css';
+import { StateContext } from '../context/stateContext';
+
+function App({ Component, pageProps }) {
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  )
 }
+
+export default App
