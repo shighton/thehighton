b439815b7478c7e119d59b9c0b7e7daa6182d9bc
voicesbygrace <voicesbygrace@gmail.com>
2023-02-15 17:52:51 -0500

    yay

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
             payment_method_types: ['card'],
             billing_address_collection: 'auto',
             shipping_options: [
-                {shipping_rate: 'shr_1MMH8CGUJRtslcEp3eV9ySEt'}
+                {shipping_rate: 'shr_1MbtpYGUJRtslcEphMF92xcc'}
             ],
             line_items: req.body.map((item) => {
                 const img = item.image[0].asset._ref;
