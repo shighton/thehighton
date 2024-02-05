63f9711d80a304d1ad25b0835b53bc4e5c41c378
shighton <dime.bastian@gmail.com>
2024-02-05 14:32:28 -0500

    Added mail capability

--- components/main/Footer.jsx
+++ components/main/Footer.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { AiFillInstagram, AiOutlineYoutube, AiOutlineLinkedin } from 'react-icons/ai';
+import { AiFillInstagram, AiOutlineYoutube, AiOutlineLinkedin, AiOutlineMail } from 'react-icons/ai';
 
 const Footer = () => {
   return (
     <div className='footer-container'>
       <p>2024 - The Highton - All Rights Reserved</p>
       <p className='icons'>
-        <a target='_blank' href='https://www.instagram.com/the.highton/'><AiFillInstagram /></a>
-        <a target='_blank' href='https://youtube.com/@shighton?si=k-kvsBW1uREqGjxZ'><AiOutlineYoutube /></a>
         <a target='_blank' href='https://www.linkedin.com/in/sabastian-highton/'><AiOutlineLinkedin /></a>
+        <a target='_blank' href="mailto:sthighton@gmail.com"><AiOutlineMail /></a>
+        {/* <a target='_blank' href='https://www.instagram.com/the.highton/'><AiFillInstagram /></a> */}
+        <a target='_blank' href='https://youtube.com/@shighton?si=k-kvsBW1uREqGjxZ'><AiOutlineYoutube /></a>
       </p>
     </div>
   )
