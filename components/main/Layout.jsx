09adf3be623b35740e4ccf9848d1f973c3da651c
Sabastian Highton <dime.bastian@gmail.com>
2025-10-24 14:51:09 -0500

    vercel analytics

--- components/main/Layout.jsx
+++ components/main/Layout.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Head from 'next/head';
 import MainNavbar from './Navbar';
 import Footer from './Footer';
+import { Analytics } from "@vercel/analytics/next"
 
 const MainLayout = ({children}) => {
   return (
     <div className='layout'>
       <Head>
         <title>Sabastian Highton</title>
+        <Analytics />
       </Head>
       <header>
         <MainNavbar />
